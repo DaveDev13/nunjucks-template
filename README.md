df0261de3c72c34a155275eac75aa4ad4765b182
DaveDev <deniskobeleff@yandex.ru>
2021-02-10 22:29:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Документация
+
+Документация к данной сборке доступна по ссылке [https://github.com/ninelines-team/ninelines-docs](https://github.com/ninelines-team/ninelines-docs).
---
+++ src/js/vendor.js
@@ -0,0 +1,10 @@
+import '@babel/polyfill';
+import svg4everybody from 'svg4everybody';
+import $ from 'jquery';
+
+svg4everybody();
+
+window.$ = $;
+window.jQuery = $;
+
+require('ninelines-ua-parser');
